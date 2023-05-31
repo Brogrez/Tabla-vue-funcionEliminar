2eadbe4274e5fbb554b52e7227a627c5de0940a4
nico <nico.mv1998@gmail.com>
2023-05-31 18:25:47 -0400

    aprendiendo a crear componentes y funcionalidad

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Tabla :juegos="games" @eliminar="eliminarJuego" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Tabla from './components/Tabla.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { 
+    Tabla
+  },
+  data(){
+    return {
+      games: ["Arena of valor", "Pokemon Unite", "Donkey Kong", "Heroes of the storm", "Valorant"]
+    }  
+  },
+  methods:{
+    eliminarJuego(id){
+      const juegosRestantes = this.games.filter((game,index) => index !== id) 
+      this.games = juegosRestantes
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Tabla.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container mt-5">
+    <table class="table w-50 mx-auto text-center">
+  <thead>
+    <tr class="table-dark">
+      <th scope="col">#</th>
+      <th scope="col">Juegos</th>
+      <th scope="col">Eliminar Juego de tu lista</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="(juego, index) in juegos" :key="index" class="table-dark table-active">
+        <td>{{index+1}}</td>
+        <td>{{juego}}</td>
+        <td><button @click="eliminarJuego(index)" type="button" class="btn btn-danger">Eliminar</button></td>
+    </tr>
+  </tbody>
+</table>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        juegos:Array,
+    },
+    methods: {
+        eliminarJuego(id){
+            this.$emit('eliminar', id)
+        }
+  }
+}
+</script>
+
+<style>
+
+</style>
